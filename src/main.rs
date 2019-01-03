5dd58d602f2425f1f38c45722d9600b41823a0b7
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-01-03 09:04:03 -0800

    Basic option example: something and nothing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    println!("");
+
+    // Complete use of Option type.
+    let number: Option<i32> = Some(5);
+    println!("number: {:?}", number);
+
+    // Compiler will auto-detect the type
+    let string = Some("some string");
+    println!("string: {:?}", string);
+
+    // Compiler can not auto-detect type of None
+    let nothing: Option<u32> = None;
+    println!("nothing: {:?}", nothing);
 }

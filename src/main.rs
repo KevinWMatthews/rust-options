18f7cc5eec2c5726f979b54a8be814a545c336fa
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-01-03 09:26:15 -0800

    Pass Option to function, match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("");
+    println!("\n\tBasic options");
 
     // Complete use of Option type.
     let number: Option<i32> = Some(5);
@@ -12,4 +12,19 @@ fn main() {
     // Compiler can not auto-detect type of None
     let nothing: Option<u32> = None;
     println!("nothing: {:?}", nothing);
+
+    println!("\n\tMatching options");
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("six: {:?}", six);
+
+    let none = plus_one(None);
+    println!("none: {:?}", none);
+}
+
+fn plus_one(number: Option<i32>) -> Option<i32> {
+    match number {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }

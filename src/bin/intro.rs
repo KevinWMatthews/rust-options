c47c958ed92d7611b3abded56cd6f031aaa0060f
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-21 16:56:40 -0700

    Update intro

--- src/bin/intro.rs
+++ src/bin/intro.rs
@@ -1,5 +1,6 @@
 fn main() {
-    /* Options are an enum variant:
+    /*
+    Options are an enum variant:
     enum Option<T> {
         Some<T>,
         None,
@@ -11,6 +12,19 @@ fn main() {
     let maybe_number: Option<i32> = Some(42);
     dbg!(maybe_number);
 
+    let maybe_number: Option<i32> = None;
+    dbg!(maybe_number);
+
+    // Types can be inferred for most Some()
+    let maybe_number = Some(4);
+    dbg!(maybe_number);
+
+    // Types cannot be inferred for None - there is nothing there!
+    // Compiler error:
+    // cannot infer type for `T`
+    /*
+    let maybe_number = None;
+    */
     let maybe_number: Option<i32> = None;
     dbg!(maybe_number);
 }
